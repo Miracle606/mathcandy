8b7a639588c5fd1b3aa39e361ce2a386614f3160
Miracle606 <Miracle606@users.noreply.github.com>
2026-08-03 16:47:39 +0000

    Auto-deployed site update from private source Miracle606/mathcandy-source@139c957958089e3be448072d953502c24c78eae8

--- javascripts/highlight-fix.js
+++
@@ -1,9 +0,0 @@
-// 阻止搜索高亮侵入Math公式块
-document$.subscribe(() => {
-  document.addEventListener('md-search:highlight', function(ev) {
-    // 不允许在 mathjax/katex 容器内部进行关键词高亮
-    ev.detail.filter = (node) => {
-      return !node.closest('.arithmatex, .MathJax, .katex');
-    };
-  });
-});
